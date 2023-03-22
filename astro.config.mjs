94ab96f2b8fd9305ae535894b79583f19c55697f
Christopher Milan <chris.m.milan@gmail.com>
2023-03-21 23:05:51 -0700

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,6 +13,9 @@ import prefetch from "@astrojs/prefetch";
 import image from "@astrojs/image";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
@@ -21,5 +24,6 @@ export default defineConfig({
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
+    sitemap(),
   ],
 });
